c9fb329fe1655186619081568161de7c4392b855
0sakana3 <shakex3.tp3@gmail.com>
2025-01-26 18:26:55 +0900

    バリデーション

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
   belongs_to  :user
   has_one_attached  :image
+
+  validates :title, presence: true
+  validates :publisher, presence: true
+  validates :detail , presence: true
+  validates :recommended, presence: true
+  validates :url, presence: true
 end
