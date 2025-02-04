f7a6e2a172f3e0929319f345f16a502fb63cb505
0sakana3 <shakex3.tp3@gmail.com>
2025-02-04 17:06:29 +0900

    ジャンルの選択内容を修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -5,7 +5,10 @@ class Genre < ActiveHash::Base
     { id: 3, name: 'コミック' },
     { id: 4, name: '文庫' },
     { id: 5, name: '新書' },
-    { id: 6, name: '単行本' }
+    { id: 6, name: '単行本（文芸書）' },
+    { id: 7, name: '単行本（実用書）' },
+    { id: 8, name: '単行本（専門書）' },
+    { id: 9, name: '単行本（児童書・学習参考書）' }
   ]
 
   include ActiveHash::Associations
