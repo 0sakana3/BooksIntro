452479110ac89e52667961bafc41df6913a764f2
0sakana3 <shakex3.tp3@gmail.com>
2025-02-04 17:39:29 +0900

    テストコード作成　Faker

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :book do
-    
+    title              { Faker::Commerce.name }
+    publisher          { Faker::Company.name }
+    genre_id           { Faker::Number.between(from: 2, to: 10) }
+    detail             { Faker::Lorem.sentence }
+    recommended        { Faker::Lorem.sentence }
+    reference          { Faker::Internet.url }
+    user 
   end
 end
