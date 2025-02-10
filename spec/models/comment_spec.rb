825774292b156c372aae34fa4dc9c40ffb9fa853
0sakana3 <shakex3.tp3@gmail.com>
2025-02-10 23:51:15 +0900

    テストコード作成　投稿できる時

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @book = FactoryBot.create(:book)
+    @comment = FactoryBot.build(:comment, user: @user, book: @book) 
+ 
+  end
+
+  describe 'コメント投稿機能' do
+    context 'コメントが投稿できる時' do
+      it 'コメントを入力して「送信」ボタンを押すと、コメントを投稿できる' do
+       expect(@comment).to be_valid
+      end
+     end
+   end
+
+   context 'コメントが投稿できない時' do
+    it 'commentが空では投稿できない' do
+    end
+    it 'ログイン状態でない場合は投稿できない' do
+    end
+    it 'コメントする本紹介ページがない場合は投稿できない' do
+    end
+    
+  end
 end
+
