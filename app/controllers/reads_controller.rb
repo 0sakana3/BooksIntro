1632d73549cac629efa0fadfba1c69fde6794f84
0sakana3 <shakex3.tp3@gmail.com>
2025-02-16 01:18:48 +0900

    読んだよ機能実装

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -8,10 +8,7 @@ class ReadsController < ApplicationController
   
   def destroy
     @read = Read.find_by(book_id: params[:book_id], user_id: current_user.id)
-    
     @read.destroy
     redirect_to root_path
   end
-  
-  end
-  
+end
