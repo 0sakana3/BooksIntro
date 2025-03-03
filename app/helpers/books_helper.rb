aca084530d06e3d223efdeb30fa44e1564c01a89
0sakana3 <shakex3.tp3@gmail.com>
2025-03-03 18:53:33 +0900

    画像表示修正

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -3,7 +3,8 @@ module BooksHelper
     if book.image.attached?
       image_tag book.image.variant(resize_to_limit: [200, 200]), class: 'book-image'
     else
-      image_tag 'default_image.png', class: 'book-image', width: 200, height: 200
+      image_tag 'https://booksintro2025-defaults.s3.ap-northeast-1.amazonaws.com/default_image.png', class: 'book-image',
+                                                                                                     width: 200, height: 200
     end
   end
 
