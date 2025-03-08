c5ad34d29f004658e84eff81aa4bd22f528368e2
0sakana3 <shakex3.tp3@gmail.com>
2025-03-09 03:00:56 +0900

    rails-ujs.js追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from "@rails/ujs"
-Rails.start()
-
 import "@hotwired/turbo-rails"
 import "controllers"
 import "./read_toggle"
 import "comment"
+
+import Rails from "@rails/ujs"
+Rails.start()
 
